1c23ab3d7e16413c2ac5eec43994c631f0c08dae
Eroshz <66910841+Eroshz@users.noreply.github.com>
2022-04-14 20:38:06 +0300

    sayitahmin.py

---
+++ KODLAR/sayitahmin.py
@@ -0,0 +1,19 @@
+import random
+
+sayilar = (1,2,3,4,5,6,7,8,9,10)
+sayi = random.randint(1, 10)
+sayi_gir = int(input("Lütfen Bir Sayı Giriniz: "))
+
+
+
+if sayi_gir == sayi :
+    print("✔️ Tebrikler")
+
+if sayi_gir > 10 :
+    print("❌ 1 İle 10 Arasında Bir Sayı Gir...")
+
+if sayi_gir < 1 :  
+    print("❌ 1 İle 10 Arasında Bir sayı Gir...")
+
+if sayi_gir != sayi : 
+    print("❌ Tekrar Dene")
