ee6367d7879915dd1c48602331a8bac0c61cf0e4
Eroshz <66910841+Eroshz@users.noreply.github.com>
2022-04-14 21:08:23 +0300

    Dört İşlem

---
+++ KODLAR/dortislem.py
@@ -0,0 +1,38 @@
+#TOPLAMA
+sayi1 = 10
+sayi2 = 20
+
+toplam = int(sayi1) + int(sayi2)
+
+print(toplam)
+#--------------------------------------------------------------------
+#ÇIKARMA
+sayi1 = 10
+sayi2 = 20
+
+toplam = int(sayi1) - int(sayi2)
+
+print(toplam)
+
+#ÇARPMA
+sayi1 = 10
+sayi2 = 20
+
+toplam = int(sayi1) * int(sayi2)
+
+print(toplam)
+
+#BÖLME
+sayi1 = 10
+sayi2 = 20
+
+toplam = int(sayi1) / int(sayi2)
+
+print(toplam)
+
+
+#FONKSİYON OLARAKA (ÖRNEK):
+def carpma(x,y) :
+    print("Cevap:" , x*y)
+
+carpma(10, 20)
